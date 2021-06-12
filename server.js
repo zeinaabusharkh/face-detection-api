702dc90f72583c181b8f358030a7b06637f98d27
Zeina <raedzeina@gmail.com>
2021-06-12 18:43:30 +0400

    more fixes

--- server.js
+++ server.js
@@ -8,23 +8,29 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const image = require('./controllers/image');
 const profile = require('./controllers/profile');
- const db = knex = require('knex')({
-	  client: 'pg',
-	  connection: {
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
-		rejectUnauthorized: false,
-		},
-		},
+const knex = require('knex'); 
+
+
+// HEROKU db connection
+const db = knex({ // for connecting to PostgreSQL
+	client: 'pg', // type of db
+	connection: { 
+	  connectionString: process.env.POSTGRES_URL, // dynamic database value from heroku hosting server    
+	  ssl: {
+		require: true,
+		rejectUnauthorized: false
+	  }
+	}
   });
 
  
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+app.use(cors()); // use 'app.use' as is middleware
+app.use(express.json()); // Used to parse JSON bodies
+app.use(express.urlencoded()); // Parse URL-encoded bodies
  
 
-app.get('/', (req, res)=>{	res.send('it is working !');})
+app.get('/', (req, res)=>{ res.send(`SmartBrain-Backend Running on port ${process.env.PORT}`)})
 app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)});
 app.post('/register', (req,res)=> {register.handleRegister(req,res,db,bcrypt)}); 
 app.get('/profile/:id', (req, res )=>{profile.handleProfile(req,res,db)});
